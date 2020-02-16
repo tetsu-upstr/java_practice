e169a09ddafef752b0bdde2c4c6edab31a4a5a2f
tetsu-upstr <bodomu@gmail.com>
2020-02-16 12:27:54 +0900

    クラスの分割

---
+++ src/code6/code225.java
@@ -0,0 +1,12 @@
+package code6;
+
+public class code225 {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 2;
+		int total = code227_CalcLogic.tasu(a, b);
+		int delta = code227_CalcLogic.hiku(a, b);
+		System.out.println("足すと" + total + "、引くと" + delta);
+	}
+	
+}
---
+++ src/code6/code227_CalcLogic.java
@@ -0,0 +1,11 @@
+package code6;
+
+public class code227_CalcLogic {
+	
+	public static int tasu(int a, int b) {
+		return(a + b);
+	}
+	public static int hiku(int a, int b) {
+		return(a - b);
+	}
+}
